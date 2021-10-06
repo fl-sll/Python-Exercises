708fcade341a4cbd4e61d05b4b984862e414273b
fl-sll <edwardalvin@gmail.com>
2021-10-06 14:11:21 +0700

    1st input

--- Python exercise 1 runway length.py
+++ Python exercise 1 runway length.py
@@ -4,4 +4,4 @@ acceleration = float(input("Enter the plane's acceleration in m/s2:"))
 runway_length = ((speed**2)/(2*acceleration))
 
 format_runway = "{:.4f}" .format(runway_length)
-print("The minimum runway length needed for this airplane is", format_runway, "meters")
+print("The minimum runway length needed for this airplane is", format_runway, 'meters')
