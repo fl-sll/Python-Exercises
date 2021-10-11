4ac96463665d258edc0ef7f5836687ce7863edfa
fl-sll <edwardalvin@gmail.com>
2021-10-11 16:29:06 +0700

    fixed discount amount

--- Python exercise 2 software sales.py
+++ Python exercise 2 software sales.py
@@ -9,10 +9,10 @@ elif x >= 50 and x <= 99:
 elif x >= 100:
     discount = 40
 else:
-    print("invalid")
+    discount = 0
 
 discount_amount = ((discount) / 100) * x * 99
-total_amount = ((x * 99) - (((discount) / 100) * x * 99))
+total_amount = ((x * 99) - discount_amount)
 format_discount_amount = "{:.2f}".format(discount_amount)
 format_total_amount = "{:.2f}".format(total_amount)
 
