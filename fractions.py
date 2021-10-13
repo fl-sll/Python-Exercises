db68adef793e1b384e1bb76319d120dad64bbf1f
fl-sll <edwardalvin@gmail.com>
2021-10-13 14:07:20 +0700

    fixed formatting

--- fractions.py
+++ fractions.py
@@ -16,13 +16,15 @@ while True:
         gcd = math.gcd(numerator, denominator)
         num1 = numerator // gcd
         den1 = denominator // gcd
+        a = num1 // den1
+        b = int(num1 % den1)
         if numerator < denominator:
-            print(numerator, "/", denominator, "is a proper fraction.")
+            print(f"{numerator}/{denominator} is a proper fraction.")
             if gcd == 1:
                 print("This proper fraction cannot be reduced any further.")
                 break
             else:
-                print("This proper fraction can be reduced to : ", num1, "/", den1)
+                print(f"This proper fraction can be reduced to : {num1}/{den1}")
                 if den1 == 1:
                     print("The whole number is", num1)
                     break
@@ -31,24 +33,20 @@ while True:
 
 
         else:
-            print(numerator, "/", denominator, "is an improper fraction.")
+            print(f"{numerator}/{denominator} is an improper fraction.")
             if gcd == 1:
                 print("This improper fraction cannot be reduced any further.")
-                a = num1 // den1
-                b = int(num1 % den1)
                 if den1 == 1:
                     print("The whole number is", num1)
                     break
                 else:
-                    print("The mixed number is", a, "and", b, "/", den1)
+                    print(f"The mixed number is {a} and {b}/{den1}")
                     break
             else:
-                print("This improper fraction can be reduced to: ", num1, "/", den1)
-                a = num1 // den1
-                b = int(num1 % den1)
+                print(f"This improper fraction can be reduced to: {num1}/ {den1}")
                 if den1 == 1:
-                    print("The whole number is", num1)
+                    print(f"The whole number is {num1}")
                     break
                 else:
-                    print("The mixed number is", a, "and", b, "/", den1)
+                    print(f"The mixed number is {a} and {b}/{den1}")
                 break
