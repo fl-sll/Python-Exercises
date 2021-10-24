d460d80aab43c89beff7fe13a7e17eff3fb53936
Edward Alvin K <68998054+fl-sll@users.noreply.github.com>
2021-10-24 15:56:25 +0700

    Update Python exercise 2 wind-chill temperature.py

--- Python Exercise 2/Python exercise 2 wind-chill temperature.py
+++ Python Exercise 2/Python exercise 2 wind-chill temperature.py
@@ -1,19 +1,20 @@
 t_a = float(input("Enter the temperature in Farenheit: "))
-
 while True:
-    if t_a >= -58 and t_a <= 41:
-        v = float(input("Enter the windspeed(mph): "))
-        
-        while v >= 2:
-            wc = 35.74 + ((0.6215) * (t_a)) - ((35.75) * (v ** 0.16)) + ((0.4275) * (t_a) * (v ** 0.16))
-            format_wc = "{:.3f}".format(wc)
-            print("The windchill index is", format_wc)
-            break
-        else:
-            print("Speed must be greater than or equal to 2")
-            v = float(input("Please re-enter the windspeed(mph): "))
-            
-        
-    else:
+    if t_a < -58 or t_a > 41:
         print("Temperature must be between -58F and 41F")
         t_a = float(input("Please re-enter the temperature in Fahrenheit: "))
+        continue
+    else:
+        break
+
+v = float(input("Please enter the windspeed: "))
+while True:
+    if v < 2:
+        print("Speed must be greater than or equal to 2")
+        v = float(input("Please re-enter the windspeed (mph): "))
+        continue
+    else:
+        wc = 35.74 + ((0.6215) * (t_a)) - ((35.75) * (v ** 0.16)) + ((0.4275) * (t_a) * (v ** 0.16))
+        format_wc = "{:.3f}".format(wc)
+        print("The windchill index is", format_wc)
+        break 
