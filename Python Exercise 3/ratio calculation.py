f96abc045432b56dde7063dea9be3e54f6df138c
Edward Alvin K <68998054+fl-sll@users.noreply.github.com>
2021-10-27 10:45:11 +0700

    Update ratio calculation.py

--- Python Exercise 3/ratio calculation.py
+++ Python Exercise 3/ratio calculation.py
@@ -1,9 +1,9 @@
 def calc_new_height():
     width = float(input("Enter the current width: "))
     height = float(input("Enter the current height: "))
-    ratio = width / height
+    ratio = width / height  #get ratio of the image
     width1 = float(input("Enter the desired width: "))
-    x = width1 / ratio
+    x = width1 / ratio #get new height from ratio and new width
     print(f"The corresponding height is: {x}")
     return
 calc_new_height()
