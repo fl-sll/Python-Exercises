60e07e0438c9e31227e022831d912ca7372b8a17
Edward Alvin K <68998054+fl-sll@users.noreply.github.com>
2021-10-27 10:44:15 +0700

    Update convert to days.py

--- Python Exercise 3/convert to days.py
+++ Python Exercise 3/convert to days.py
@@ -4,14 +4,14 @@ def convert_to_days():
     seconds = float(input("Enter the number of seconds: "))
     return hours, minutes, seconds
 
-def get_days(hours,minutes,seconds):
+def get_days(hours,minutes,seconds): #convert hours,mins and seconds to days and add them together
     h1 = hours/24 
     m1 = minutes/(60*24)
     s1 = seconds/(60*60*24)
     days = h1 + m1 + s1
     return days
 
-def main():
+def main(): #format and print the number of days
     h,m,s = convert_to_days()
     days = get_days(h,m,s)
     days1 = round(days, 4)
