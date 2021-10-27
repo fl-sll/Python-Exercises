548bb2bad753d7e2bc038661efeb3e41a2bd2b82
Edward Alvin K <68998054+fl-sll@users.noreply.github.com>
2021-10-27 10:42:57 +0700

    Update atom calculation.py

--- Python Exercise 3/atom calculation.py
+++ Python Exercise 3/atom calculation.py
@@ -1,6 +1,6 @@
 def num_atoms(mass, weight=196.97):
-    atoms = mass / weight
-    return atoms * (6.022 * (10 ** 23))
+    atoms = mass / weight #number of atoms
+    return atoms * (6.022 * (10 ** 23)) #number of moles
 
 x = num_atoms(10)
 print(x)
