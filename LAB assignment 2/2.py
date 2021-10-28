1933bf2d149db142953ed9d7e7dcb0d007d9b022
fl-sll <edwardalvin@gmail.com>
2021-10-28 09:10:21 +0700

    fixed loop

--- LAB assignment 2/2.py
+++ LAB assignment 2/2.py
@@ -1,11 +1,8 @@
 def pyramid():
     height = int(input("Enter height of pyramid: "))
     for i in range(height):
-        print(" " * (height-i), "*" * (i+1), end = "")
-        print("*" * (i), " " * (height-i), end="")
-        print()
+        print(" " * (height-i), "*" * (2*i + 1))
     for i in range(height-2, -1, -1):
-        print(" " * (height-i), "*" * (i+1), end = "")
-        print("*" * (i), " " * (height-i), end="")
-        print()
+        print(" " * (height - i), "*" * (2*i + 1))
+    print()
 pyramid()
