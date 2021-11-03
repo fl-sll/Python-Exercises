01f07abedeb62df2c432ca3091dcb38a1c3c7182
fl-sll <edwardalvin@gmail.com>
2021-11-03 14:13:26 +0700

    first commit

---
+++ LAB assignment 3/2.py
@@ -0,0 +1,13 @@
+dict = {
+    "Alvin": ["Innova", "M2", "GLS600", "Palisade"],
+    "Aaron": ["CRV", "Ertiga", "G63"],
+    "Michelle": ["Avanza", "Gran Max", "Terios"],
+    "Angela": ["Accord", "Fortuner", "Pajero"],
+    "Felix": ["Camry", "Captiva", "Camaro"]
+}
+
+for key in dict.keys():
+    print(f"{key} likes these cars:")
+    for element in list(dict.get(key)):
+        print("-", element)
+    print()  # to make it neater
