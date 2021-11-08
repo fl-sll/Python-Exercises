b5c049b52ea3d1291a1c26fb8f131472283a630e
fl-sll <edwardalvin@gmail.com>
2021-11-08 12:00:59 +0700

    added comments

--- List Programming Exercises/1.py
+++ List Programming Exercises/1.py
@@ -3,7 +3,9 @@ import re
 
 def hapax(filepath):
     file = open(filepath, encoding="utf8")
-    words = re.findall('\w+', file.read().lower())
+    words = re.findall('\w+', file.read().lower()) #'\w+' find the first alphanumeric character 
+                                                    #and then check the next character and if it is alphanumeric 
+                                                    # then include it in the match 
     freqs = {key: 0 for key in words}
     for word in words:
         freqs[word] += 1
