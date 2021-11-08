e84180d49643c9c3875382f2b692736fc4f00769
fl-sll <edwardalvin@gmail.com>
2021-11-08 12:10:29 +0700

    added comments

--- List Programming Exercises/1.py
+++ List Programming Exercises/1.py
@@ -6,11 +6,11 @@ def hapax(filepath):
     words = re.findall('\w+', file.read().lower()) #'\w+' find the first alphanumeric character 
                                                     #and then check the next character and if it is alphanumeric 
                                                     # then include it in the match 
-    freqs = {key: 0 for key in words}
-    for word in words:
-        freqs[word] += 1
+    freqs = {key: 0 for key in words}  #dict for counting the key/word
+    for word in words:  
+        freqs[word] += 1  # input words into the dict and count it
     for word in freqs:
-        if freqs[word] == 1:
+        if freqs[word] == 1:    # if word is more than 1, it will not be printed as it is not a hapax
             print(word)
 
 hapax('Gutenberg.txt')
