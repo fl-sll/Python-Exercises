3e0b1324cbfe26f9a308cf7a8c07f0d86564f36a
fl-sll <edwardalvin@gmail.com>
2021-11-09 16:51:02 +0700

    added comments

--- Files Programming Exercises/3.py
+++ Files Programming Exercises/3.py
@@ -2,8 +2,8 @@ import re
 
 def avgword(file):
     file = open(file, encoding="utf8")
-    words = re.findall('\w+', file.read())
-    avg = sum([len(word) for word in words]) / len(words)
-    print("The average word length is", "{:.2f}".format(avg))
+    words = re.findall('\w+', file.read()) #get all wprds from the files
+    avg = sum([len(word) for word in words]) / len(words) #calculate the average length of words
+    print("The average word length is", "{:.2f}".format(avg)) #format it to 2 decimal places
 
 avgword('Gutenberg.txt')
