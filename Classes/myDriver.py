61348c73a4f694d640a6fb9212563ae44ea22c65
fl-sll <edwardalvin@gmail.com>
2021-11-29 11:43:56 +0700

    commit

---
+++ Classes/myDriver.py
@@ -0,0 +1,13 @@
+from myClasses import Shark
+
+def main():
+    sammy = Shark("Sammy", 5)
+    sammy.swim()
+    sammy.be_awesome()
+    sammy.old()
+    steve = Shark("Steve", 10)
+    steve.swim()
+    steve.be_awesome()
+    steve.old()
+
+main()
