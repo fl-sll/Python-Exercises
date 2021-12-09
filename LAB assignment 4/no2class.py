98646b6f9b350f77d6ff8f3035b1f2a683e269e0
fl-sll <edwardalvin@gmail.com>
2021-12-09 21:19:50 +0700

    fixed double

--- LAB assignment 4/no2class.py
+++ LAB assignment 4/no2class.py
@@ -2,7 +2,7 @@ import math
 
 class Circle:
     def __init__(self,radius = 1.0,color = "red"):
-        self.__radius = radius ** 2
+        self.__radius = radius * 2
         self.__color = str(color)
     
     def getRadius(self):
@@ -12,10 +12,10 @@ class Circle:
         return self.__color
     
     def getArea(self):
-        return math.pi * self.__radius
+        return math.pi * (self.__radius ** 2)
 
     def setRadius(self, new_radius):
-        self.__radius = new_radius ** 2
+        self.__radius = new_radius * 2
 
     def setColor(self, new_color):
         self.__color = new_color
@@ -27,13 +27,13 @@ class Circle:
 class Cylinder(Circle):
     def __init__(self, radius, color, height = 1.0):
         super().__init__(radius, color)
-        self.__height = height ** 2
+        self.__height = height * 2
 
     def getHeight(self):
         return self.__height
     
     def setHeight(self, new_height):
-        self.__height = new_height ** 2
+        self.__height = new_height * 2
     
     def getVolume(self):
         return Circle.getArea(self) * self.__height
